67345dc649c3cde48be8caa076c449f811fe7073
Marvin Clerge <clergemarvin@gmail.com>
2018-01-15 01:40:38 -0500

    Comment section implemented, need to clean code

--- challenge.js
+++ challenge.js
@@ -1,19 +1,24 @@
 const store = {}
+let isPaused = false
 
 document.addEventListener('DOMContentLoaded', function(event) {
 
   // COUNTER INCREMENTING
   const counter = document.getElementById('counter');
   let interval = setInterval(function() {
+    if (isPaused === false) {
       let num = parseInt(counter.innerHTML)
       counter.innerHTML = ++num
+    }
    }, 1000);
 
    // PLUS BUTTON
    const pButton = document.getElementById('+');
    pButton.addEventListener('click', function(event) {
-     let num = parseInt(counter.innerHTML)
-     counter.innerHTML = ++num
+     if (isPaused === false) {
+       let num = parseInt(counter.innerHTML)
+       counter.innerHTML = ++num
+     }
    })
 
    // MINUS BUTTON
@@ -27,29 +32,54 @@ document.addEventListener('DOMContentLoaded', function(event) {
    // LIKE BUTTON
    const hButton = document.getElementById('<3');
    hButton.addEventListener('click', function(event) {
-    const pTags = document.getElementsByTagName('p')
-    const number = parseInt(counter.innerHTML)
+    if (isPaused === false) {
+      const pTags = document.getElementsByTagName('p')
+      const number = parseInt(counter.innerHTML)
 
-    if (store[number] === undefined) {
-      store[number] = 1
-    } else {
-      ++store[number]
+      if (store[number] === undefined) {
+        store[number] = 1
+      } else {
+        ++store[number]
+      }
+
+      document.querySelector('ul').remove()
+      const list = document.createElement('ul')
+      document.body.appendChild(list)
+
+      for (const element in store) {
+        let listItem = document.createElement('li')
+        listItem.innerHTML = `${element} with ${store[element]}`
+        list.appendChild(listItem)
+      }
     }
+  })
 
-    document.querySelector('ul').remove()
-    const list = document.createElement('ul')
-    document.body.appendChild(list)
 
-    for (const element in store) {
-      let listItem = document.createElement('li')
-      listItem.innerHTML = `${element} with ${store[element]}`
-      list.appendChild(listItem)
-    }
+   // PAUSE BUTTON
+   const pause = document.getElementById('pause')
+   pause.addEventListener('click', function() {
+     if (isPaused === false) {
+       isPaused = true;
+       pause.innerHTML = 'resume'
+     } else {
+       isPaused = false;
+       pause.innerHTML = 'pause'
+     }
    })
 
 
+   // COMMENTS
+   const input = document.getElementById('input')
+   const submit = document.getElementById('submit')
+   const commentList = document.getElementById('list')
 
-
-
-
+   submit.addEventListener('click', function(event){
+     event.preventDefault()
+     if (isPaused === false) {
+       let comment = document.createElement('p')
+       comment.innerHTML = input.value
+       commentList.appendChild(comment)
+       input.value = ''
+     }
+   })
 })
